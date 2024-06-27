9be69b311f72a93bdde6d84112736f9c0d4cd68b
agigibairyn <ireneagigiba>
2024-06-27 14:13:28 +0200

    Self examination

--- pink_ribbon/lib/self_examination.dart
+++ pink_ribbon/lib/self_examination.dart
@@ -13,9 +13,17 @@ class SelfExamination extends StatelessWidget {
             children: [
               const Padding(
                 padding: EdgeInsets.only(top: 30),
-                child: Text(
-                  'Self Examination',
-                  style: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
+                child: Row(
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.only(right: 30.0), // Adjusted margin to the right of the icon
+                      child: Icon(Icons.menu),
+                    ),
+                    Text(
+                      'Self Examination',
+                      style: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold, color: Color(0xFFEC407A)),
+                    ),
+                  ],
                 ),
               ),
               const Padding(
@@ -25,33 +33,38 @@ class SelfExamination extends StatelessWidget {
                   style: TextStyle(fontSize: 18.0),
                 ),
               ),
-              _content(context, 'a', 'assets/images/Calendar.png',
-                'Breast Self Exam should be done once  a month within 2-3 days after menstruation or any day in a month by those who do not have menstruation.'
+              _content(context, 'a', 'images/Calendar.png',
+                'Breast Self Exam should be done once a month within 2-3 days after menstruation or any day in a month by those who do not have menstruation.'
               ),
-              _content(context, 'b', 'assets/images/hand.jpg',
+              _content(context, 'b', 'images/hand.jpg',
                 'Use the pads of your three middle fingers. To check your entire breast area, start at your armpit with varying degrees of pressure by moving in a circular motion'
               ),
-              _content(context, 'c', 'assets/images/BSE-rubbing.png',
+              _content(context, 'c', 'images/BSE-rubbing.png',
                 'Move your fingers up and down your entire breast area, from your collarbone down to your breastbone and under your armpits, vice versa'
               ),
               const Padding(
                 padding: EdgeInsets.all(8.0),
                 child: Text(
                   'Steps for Self Examination',
-                  style: TextStyle(fontSize: 18.0, color: Color(0xFFEC407A)),
+                  style: TextStyle(fontSize: 18.0, color: Color(0xFFEC407A), fontWeight: FontWeight.bold),
                   textAlign: TextAlign.center,
                 ),
               ),
-              _content(context, '1', 'assets/images/Step-1.webp',
-                'Step 1:  Begin by looking at your breasts in the mirror with your shoulders straight and your arms on your hips.  Here’s what you should look for:  Breasts that are their usual size, shape, and color  Breasts that are evenly shaped without visible distortion or swelling'),
-              _content(context, '2', 'assets/images/Step-2.webp',
-                  'Step 2:  Raise Arms and Examine Your Breasts Now, raise your arms and look for the same changes.'),
-              _content(context, '3', 'assets/images/Step-1.webp',
-                  'Step 3:  While you’re at the mirror, look for any signs of fluid coming out of one or both nipples (this could be a watery, milky, or yellow fluid or blood).'),
-              _content(context, '4', 'assets/images/Step-4.webp',
-                  'Step 4:  Feel Your Breasts for Lumps While Standing or Sitting Finally, feel your breasts while you are standing or sitting. Many women find that the easiest way to feel their breasts is when their skin is wet and slippery, so they like to do this step in the shower. Cover your entire breast, using the same hand movements.'),
-              _content(context, '5', 'assets/images/Step-5.webp',
-                  'Step 5:  Feel for Breast Lumps While Lying Down Next, check for breast lumps or abnormalities by feeling your breasts while lying down, using your right hand to feel your left breast, and then your left hand to feel your right breast. Use a firm, smooth touch with the first few finger pads of your hand, keeping the fingers flat and together.'),
+              _content(context, '1', 'images/Step-1.webp',
+                'Step 1: Begin by looking at your breasts in the mirror with your shoulders straight and your arms on your hips. Here’s what you should look for: Breasts that are their usual size, shape, and color Breasts that are evenly shaped without visible distortion or swelling'
+              ),
+              _content(context, '2', 'images/Step-2.webp',
+                'Step 2: Raise Arms and Examine Your Breasts Now, raise your arms and look for the same changes.'
+              ),
+              _content(context, '3', 'images/Step-1.webp',
+                'Step 3: While you’re at the mirror, look for any signs of fluid coming out of one or both nipples (this could be a watery, milky, or yellow fluid or blood).'
+              ),
+              _content(context, '4', 'images/Step-4.webp',
+                'Step 4: Feel Your Breasts for Lumps While Standing or Sitting Finally, feel your breasts while you are standing or sitting. Many women find that the easiest way to feel their breasts is when their skin is wet and slippery, so they like to do this step in the shower. Cover your entire breast, using the same hand movements.'
+              ),
+              _content(context, '5', 'images/Step-5.webp',
+                'Step 5: Feel for Breast Lumps While Lying Down Next, check for breast lumps or abnormalities by feeling your breasts while lying down, using your right hand to feel your left breast, and then your left hand to feel your right breast. Use a firm, smooth touch with the first few finger pads of your hand, keeping the fingers flat and together.'
+              ),
             ],
           ),
         ),
@@ -70,7 +83,7 @@ class SelfExamination extends StatelessWidget {
               fontSize: 16.0, 
               fontWeight: FontWeight.bold,
               color: isNumeric(label) ? const Color(0xFFEC407A) : Colors.black
-              ),
+            ),
             textAlign: TextAlign.center,
           ),
         ),
@@ -91,7 +104,6 @@ class SelfExamination extends StatelessWidget {
     );
   }
 }
-
 
 bool isNumeric(String? value) {
   if (value == null || value.isEmpty) {
