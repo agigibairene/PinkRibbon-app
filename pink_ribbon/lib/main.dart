3c27c68e59d6dc8717ec6580510a233306382eaf
agigibairene <116544268+agigibairene@users.noreply.github.com>
2024-06-30 14:37:23 +0200

    Update main.dart

--- pink_ribbon/lib/main.dart
+++ pink_ribbon/lib/main.dart
@@ -18,7 +18,7 @@ void main() async {
     const Duration(seconds: 2),
   );
   FlutterNativeSplash.remove();
-  runApp(const LoginPage());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatefulWidget {
